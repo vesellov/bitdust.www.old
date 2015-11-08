7d414340507e49a40cb2d59ae63cc42065f3459e
Veselin <penev.veselin@gmail.com>
2015-11-08 22:29:49 +0300

    seems like feedback is looking much better, let's try

--- feedback.php
+++ feedback.php
@@ -2,85 +2,45 @@
     
     ini_set("display_errors", 1); 
         
-    if(isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
-      if (isset($_POST['message']) AND isset($_POST['address'])) {
-        $to = 'bitdust.io@gmail.com';
+    if( !isset($_SERVER['HTTP_X_REQUESTED_WITH']) ) {
+        return;
+    }
+    
+    if ( trim($_POST['valTrFal']) != 'valTrFal_true' ) {
+        echo 'Need to Confirm your post.';
+        return;
+    }
 
-        $address = filter_var($_POST['address'], FILTER_SANITIZE_STRING);
-        $message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
-
-        $sent = email($to, $address, $message);
-        if ($sent) {
-          echo 'Message sent!';
-        } else {
-          echo 'Message couldn\'t sent!';
-        }
-      } else {
+    if( !isset($_POST['txtname']) || !isset($_POST['txtemail']) || !isset($_POST['message']) ) {  
         echo 'All Fields are required';
-      }
-      return;
+        return;
+    }
+    
+    $to = 'bitdust.io@gmail.com';
+    $txtname = filter_var($_POST['txtname'], FILTER_SANITIZE_STRING);
+    $themsLat = 'message received from [bitdust.io]';
+    $txtemail = filter_var($_POST['txtemail'], FILTER_SANITIZE_STRING);
+    $comments = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
+
+    $b = '<html><head>';
+    $b = $b . '<title>' . $themsLat . '</title>';
+    $b = $b . '<p>IP: <b>' . $_SERVER['REMOTE_ADDR'] . '</b></p>';
+    $b = $b . '<p>Name: <b>' . $txtname . '</b></p>';
+    $b = $b . '<p>E-mail: <b>' . $txtemail . '</b></p>';
+    $b = $b . '<br><p>' . $comments . '</p>';
+    $b = $b . '</head><body>';
+
+    $header = array();
+    $header[] = "MIME-Version: 1.0";
+    $header[] = "From: {$to}<{$to}>";
+    $header[] = "Content-type:text/html; charset=utf-8";
+    $header[] = "Content-Transfer-Encoding: 7bit";
+    
+    if ( !mail($to, 'message received from [bitdust.io]', $b, implode("\r\n", $header)) ) {
+        echo 'Message couldn\'t sent!';
+        return; 
     }
 
-    function email($to, $address, $message){
-      $header = array();
-      $header[] = "MIME-Version: 1.0";
-      $header[] = "From: {$to}<{$to}>";
-      $header[] = "Content-type:text/html; charset=iso-8859-1";
-      $header[] = "Content-Transfer-Encoding: 7bit";
-      $body = '<html><head><title>message received from [bitdust.io]</title></head><body>';
-      $body = $body . '<br>IP: <b>' . $_SERVER['REMOTE_ADDR'] . '</b><br>';
-      $body = $body . '<br><b>' . $address . '</b><br>';
-      $body = $body . '<br>Message: <br><br>' . $message;
-      if(mail($to, 'message received from [bitdust.io]', $body, implode("\r\n", $header)))
-          return true; 
-      return false;
-    }
+    echo 'Message sent.';
   
- 
-/*      
-        
-    //if(isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
-    if (1) {
-        
-        if (trim($_POST['valTrFal'])!='valTrFal_true') {
-        
-            echo '<script language="JavaScript">window.location.href="/";</script>';
-            //return false;
-            
-        } else {
-        
-            if( isset($_POST['btnsend']) ) {
-                    $mailto = "bitdust.io@gmail.com"; 
-                    $charset = 'utf-8';
-                    $headers = "Content-type: text/plain; charset={$charset}\n";		
-                    $headers .= "From: bitdust.io@gmail.com";
-                    //$txtname = trim($_POST['txtname']);
-                    $txtname = filter_var($_POST['txtname'], FILTER_SANITIZE_STRING);
-                    $themsLat = 'message received from [bitdust.io]';
-                    //$txtemail = trim($_POST['txtemail']);
-                    $txtemail = filter_var($_POST['txtemail'], FILTER_SANITIZE_STRING);
-                    //$comments = trim($_POST['message']);
-                    $comments = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
-
-                    $body = $themsLat . '\n\n';
-                    $body = $body . 'IP: ' . $_SERVER['REMOTE_ADDR'] . '\n\n';
-                    $body = $body . 'Name: ' . $txtname . '\n\n';
-                    $body = $body . 'E-mail: ' . $txtemail . '\n\n';
-                    $body = $body . 'Message: \n\n' . $comments; 
-              
-                    mail($mailto, $themsLat, $body, $headers);
-                    //mail($mailto,'=?'.$charset.'?B?'.base64_encode($themsLat).'?=',$body,$headers);
-            
-                    echo '<script language="JavaScript">window.location.href="/";</script>';
-                    //return true;
-
-            } else {
-                echo '<script language="JavaScript">window.location.href="/";</script>';
-                //return false;
-            }
-        }
-    } else {
-        return false;
-    }
-*/
 ?>
